576cc8036fc07d01366f2990552d68547cd5a0d4
kambojianveshkumar <146006414+kambojianveshkumar@users.noreply.github.com>
2025-12-07 17:14:00 +0530

    Update app.py

--- consumer/app.py
+++ consumer/app.py
@@ -1,1 +1,22 @@
+import json
+from kafka import KafkaConsumer
+
+KAFKA_BROKER = "kafka:9092"
+TOPIC_NAME = "orders-topic"
+
+def main():
+    consumer = KafkaConsumer(
+        TOPIC_NAME,
+        bootstrap_servers=KAFKA_BROKER,
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+    )
+
+    print("Kafka Order Consumer Started...")
+    for msg in consumer:
+        print(f"Received Order: {msg.value}")
+
+if __name__ == "__main__":
+    main()
 
